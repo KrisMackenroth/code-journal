6e21517fd0ac4e5e54254003558117d1ef2b6650
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-05-31 04:09:55 +0000

    test.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var urlInput = document.querySelector('.user-url');
+
+var newImage = document.querySelector('.new-image');
+
+var entryForm = document.querySelector('.journal-entry-form');
+
+urlInput.addEventListener('input', function (event) {
+
+  var test = event.target.value;
+
+  newImage.setAttribute('src', test);
+});
+
+entryForm.addEventListener('submit', function (e) {
+  var newObject = {};
+  newObject.title = entryForm.title.value;
+  newObject.photoUrl = entryForm.url.value;
+  newObject.notes = entryForm.notes.value;
+  newObject.EntryId = data.nextEntryId;
+
+  data.entries.unshift(newObject);
+  localStorage.setItem('entries', JSON.stringify(data));
+});
