5f51a3b9941af739552cc4b8fae831bc7bef5891
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-06-01 00:22:18 +0000

    Code now allows user to view entries as well as switch between new entry page and entries page.

--- js/main.js
+++ js/main.js
@@ -19,4 +19,66 @@ entryForm.addEventListener('submit', function (e) {
   newObject.EntryId = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(newObject);
+  var secondContainer = document.querySelector('.entries-button');
+  var firstContainer = document.getElementById('first-container');
+  secondContainer.classList.remove('hidden');
+  firstContainer.classList.add('hidden');
+});
+
+window.addEventListener('DOMContentLoaded', function (e) {
+  var ul = document.querySelector('ul');
+  var storage = localStorage.getItem('Entries');
+  var obj = JSON.parse(storage);
+  var entriesTest = obj.entries;
+  for (var i = 0; i < entriesTest.length; i++) {
+    var allEntries = journalEntry(entriesTest[i]);
+    ul.appendChild(allEntries);
+  }
+  function journalEntry(entry) {
+    var li = document.createElement('li');
+    var divRow = document.createElement('div');
+    divRow.classList.add('row');
+    li.appendChild(divRow);
+    var divColumnFullHalf = document.createElement('div');
+    divColumnFullHalf.classList.add('column-full');
+    divColumnFullHalf.classList.add('column-half');
+    divRow.appendChild(divColumnFullHalf);
+    var image = document.createElement('img');
+    image.classList.add('new-image');
+    image.setAttribute('src', entriesTest[i].photoUrl);
+    image.setAttribute('alt', 'placeholder');
+    divColumnFullHalf.appendChild(image);
+    var divText = document.createElement('div');
+    divText.classList.add('column-full');
+    divText.classList.add('column-half');
+    divText.classList.add('text-input');
+    divRow.appendChild(divText);
+    var name = document.createElement('h1');
+    var nameText = document.createTextNode(entriesTest[i].title);
+    name.appendChild(nameText);
+    divText.appendChild(name);
+    var p = document.createElement('p');
+    var pText = document.createTextNode(entriesTest[i].notes);
+    p.appendChild(pText);
+    divText.appendChild(p);
+    return li;
+  }
+});
+
+var anchor = document.querySelector('.anchor');
+
+var clickButton = document.querySelector('.click-button');
+
+clickButton.addEventListener('click', function (event) {
+  var secondContainer = document.querySelector('.entries-button');
+  var firstContainer = document.getElementById('first-container');
+  secondContainer.classList.add('hidden');
+  firstContainer.classList.remove('hidden');
+});
+
+anchor.addEventListener('click', function (event) {
+  var secondContainer = document.querySelector('.entries-button');
+  var firstContainer = document.getElementById('first-container');
+  secondContainer.classList.remove('hidden');
+  firstContainer.classList.add('hidden');
 });
